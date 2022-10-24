939287f9b3d1d833a1cfcf06c102163dda42fac3
yuchan <74092268+H-yuchan@users.noreply.github.com>
2022-10-24 15:01:43 +0900

    3단원까지 끝냄.

--- Study_C/main.c
+++ Study_C/main.c
@@ -52,3 +52,79 @@ int main(){
 }
 */
 
+//03-1[scanf 함수의 활용]
+
+//03-1 문제1 : 사용자로부터 두 개의 정수를 입력 받아서 두 수의 뺄셈과 곱셈의 결과를 출력하는 프로그램을 작성하시오.
+/*
+#include <stdio.h>
+
+int main(){
+    int n1=0, n2=0;
+    
+    scanf("%d %d",&n1,&n2);
+    
+    printf("%d-%d=%d\n",n1,n2,n1-n2);
+    printf("%d*%d=%d\n",n1,n2,n1*n2);
+    
+    
+    return 0;
+}
+*/
+
+
+/*
+//03-1 문제2 : 사용자로부터 세 개의 정수 num1, num2, num3를 순서대로 입력 받은 후에, 다음 연산의 결과를 출력하는 프로그램을 작성해보자.
+#include <stdio.h>
+int main(){
+    
+    int num1=0, num2=0, num3=0;
+    scanf("%d %d %d",&num1, &num2, &num3);
+    printf("%dx%d+%d=%d\n", num1,num2, num3,num1*num2+num3);
+    return 0;
+}
+*/
+
+/*
+//03-1 문제3 : 하나의 정수를 입력 받아서, 그 수의 제곱의 결과를 출력하는 프로그램을 작성해보자. 예를 들어서 5가 입력되면 25가 출력된다.
+#include <stdio.h>
+int main(){
+    int n=0;
+    scanf("%d",&n);
+    printf("%d\n",n*n);
+    
+    
+    return 0;
+}
+*/
+
+
+/*
+//03-1 문제4 : 입력 받은 두 정수를 나누었을 때 얻게 되는 몫과 나머지를 출력하는 프로그램을 작성해보자. 예를 들어 7과 2가 입력되면 몫으로 3, 나머지로 1이 출력되어야 한다.
+#include <stdio.h>
+
+int main(){
+    int n1, n2;
+    scanf("%d %d",&n1, &n2);
+    
+    printf("몫 : %d, 나머지 : %d \n", n1/n2, n1%n2);
+    return 0;
+}
+*/
+
+/*
+//03-1 문제5 : 입력 받은 세 개의 정수 num1, num2, num3를 대상으로 다음 연산의 결과를 출력하는 프로그램을 작성해보자/
+#include <stdio.h>
+
+int main(){
+    int num1, num2, num3;
+    scanf("%d %d %d", &num1, &num2, &num3);
+    printf("(%d-%d)(%d+%d)(%d%%%d) = %d \n", num1,num2, num2, num3,num3, num1, (num1-num2)*(num2+num3)*(num3%num1) );
+    return 0;
+}
+*/
+
+
+
+
+
+
