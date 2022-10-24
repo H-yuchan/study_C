e3e0bf551569a6b24d534c35dc40f64c645d4a5b
yuchan <74092268+H-yuchan@users.noreply.github.com>
2022-10-24 10:49:28 +0900

    기본세팅

---
+++ Study_C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Study_C
+//
+//  Created by YU CHAN HWANG on 2022/10/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
